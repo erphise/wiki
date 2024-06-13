de23f93ca33412c01f75c4d383ced5406642545a
erphise <github@erphise.com>
2024-06-13 15:21:34 +0200

    Changed docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'erphise', // Usually your GitHub org/user name.
   projectName: 'wiki', // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
